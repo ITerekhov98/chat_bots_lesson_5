d9c6a41ae4a6bf48f46fd5bd4f465ac3b4a1fe45
iterekhov98@gmail.com <iterekhov98@gmail.com>
2022-06-27 17:32:35 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,87 @@
+import requests
+from environs import Env
+
+
+def get_access_token(client_id, client_secret):
+    url = 'https://api.moltin.com/oauth/access_token'
+    data = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'client_credentials',
+    }
+    response = requests.post(url, data=data)
+    response.raise_for_status()
+    return response.json()['access_token']
+
+
+def get_all_products(token):
+    url = 'https://api.moltin.com/v2/products'
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def create_cart(token, tg_user_id):
+    url = 'https://api.moltin.com/v2/carts'
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json'
+    }
+    json_data = {
+        'data': {
+            'name': f'cart for user {tg_user_id}',
+            'id': tg_user_id
+        }
+    }
+    response = requests.post(url, headers=headers, json=json_data)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_cart(token, user_id):
+    url = f'https://api.moltin.com/v2/carts/{user_id}'
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_cart_items(token, user_id):
+    url = f'https://api.moltin.com/v2/carts/{user_id}/items'
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }    
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()    
+
+
+def add_product_to_cart(token, user_id, product_sku):
+    url = f'https://api.moltin.com/v2/carts/{user_id}/items'
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json',
+    }
+    json_data = {
+        'data': {
+            'sku': product_sku,
+            'type': 'cart_item',
+            'quantity': 1
+        }
+    }
+    response = requests.post(url, headers=headers, json=json_data)
+    response.raise_for_status()
+    return response.json()
+
+
+def main():
+    env = Env()
+    env.read_env()
+    client_id = env.str('ELASTIC_PATH_CLIENT_ID')
+    client_secret = env.str('ELASTIC_PATH_CLIENT_SECRET')
+    cms_token = get_access_token(client_id, client_secret)
